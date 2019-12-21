8ca87c566136f6f06bbecdb5238e5cc3e00295e0
Shiwangi1995 <59121105+Shiwangi1995@users.noreply.github.com>
2019-12-22 00:04:46 +0530

    test1

---
+++ Interview/ifcheck.java
@@ -0,0 +1,21 @@
+package Interview;
+
+public class ifcheck {
+
+	public static void main(String[] args) {
+		int n=15;
+		for(int i=2;i<n;i++) {
+
+			if(n%i==0)
+			{
+				System.out.println(" yeyee");
+				
+			}
+		}
+		
+		
+			System.out.println(" no");
+		
+
+	}
+}
